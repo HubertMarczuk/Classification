43d78c26803e4c662944aa5d32986997642b9e7c
Hubert Marczuk <dziabongjp@gmail.com>
2023-11-22 15:58:59 +0100

    added comments changed localization

--- classify.py
+++ classify.py
@@ -1,46 +1,51 @@
 from sklearn.neighbors import KNeighborsClassifier
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.model_selection import train_test_split
-from sklearn.metrics import accuracy_score
 from preparedata import Prepare
 
 
+# Funkcja przeprowadzająca klasyfikację KNN
 def KNN(X_train, X_test, Y_train, Y_test, K):
+    # Dopasowanie klasyfikatora KNN dla danych treningowych
     knn = KNeighborsClassifier(n_neighbors=K)
-
     knn.fit(X_train, Y_train)
 
+    # Przewidywanie klasy dla danych testowych
     Y_pred = knn.predict(X_test)
 
+    # Zwrócenie listy z przewidzianaymi klasami dla każdego rekordu
     return Y_pred
 
 
+# Funkcja przeprowadzająca klasyfikację Random Forest
 def RandomForest(X_train, X_test, Y_train, Y_test, Trees):
+    # Dopasowanie klasyfikatora Random forest dla danych treningowych
     rf = RandomForestClassifier(n_estimators=Trees)
-
     rf.fit(X_train, Y_train)
 
+    # Przewidywanie klasy dla danych testowych
     Y_pred = rf.predict(X_test)
 
+    # Zwrócenie listy z przewidzianaymi klasami dla każdego rekordu
     return Y_pred
 
 
-X, Y = Prepare("Classification/magic04.data")
+# Przygotowanie danych do klasyfikacji
+X, Y = Prepare("magic04.data")
 
-K = 5
+# Wybór liczby najbliższych sąsiadów oraz liczby drzeww lesie
+K = 10
+Trees = 50
 
-Trees = 1000
-
+# Losowy podział zbioru treningowego i testowego
 X_train, X_test, Y_train, Y_test = train_test_split(X, Y, test_size=0.4)
 
+# Klasyfikowanie
 Y_pred_KNN = KNN(X_train, X_test, Y_train, Y_test, K)
-
 Y_pred_RandomForest = RandomForest(X_train, X_test, Y_train, Y_test, Trees)
 
-
+# Obliczanie wartości macierzy klasyfikacji dla modelu wykorzystującego KNN
 TP1, FP1, FN1, TN1 = 0, 0, 0, 0
-TP2, FP2, FN2, TN2 = 0, 0, 0, 0
-
 for i in range(len(Y_pred_KNN)):
     if Y_test[i] == 0 and Y_pred_KNN[i] == 0:
         TN1 += 1
@@ -51,6 +56,8 @@ for i in range(len(Y_pred_KNN)):
     else:
         TP1 += 1
 
+# Obliczanie wartości macierzy klasyfikacji dla modelu wykorzystującego Random Forest
+TP2, FP2, FN2, TN2 = 0, 0, 0, 0
 for i in range(len(Y_pred_RandomForest)):
     if Y_test[i] == 0 and Y_pred_RandomForest[i] == 0:
         TN2 += 1
@@ -61,6 +68,7 @@ for i in range(len(Y_pred_RandomForest)):
     else:
         TP2 += 1
 
+# Wyświetlenie wyników i obliczenie miar klasyfikacji dla KNN
 print("-----Klasyfikator KNN-----")
 print("Wartości macierzy klasyfikacji:")
 print("TP =", TP1, "   ", "FP =", FP1)
@@ -69,6 +77,8 @@ print("Trafność =", (TP1 + TN1) / (TP1 + TN1 + FP1 + FN1))
 print("Czułość =", TP1 / (TP1 + FN1))
 print("Specyficzność =", TN1 / (TN1 + FP1))
 print("-----------------------------")
+
+# Wyświetlenie wyników i obliczenie miar klasyfikacji dla Random Forest
 print("-----Klasyfikator Random Forest-----")
 print("Wartości macierzy klasyfikacji:")
 print("TP =", TP2, "   ", "FP =", FP2)
